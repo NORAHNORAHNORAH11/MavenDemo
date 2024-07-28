dc6bf4f129d0e3d89b4ab2db8385b042671b10f8
norha <norahshrestha11@gmail.com>
2024-07-28 14:51:12 +0545

    added

---
+++ src/main/java/bway/MAVENDEMO/App.java
@@ -0,0 +1,13 @@
+package bway.MAVENDEMO;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
